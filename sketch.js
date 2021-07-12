85a75964ee132f686cf32514dc2599e22169059c
karthik008-cod <72755692+karthik008-cod@users.noreply.github.com>
2021-07-12 13:40:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var M1, M2, M3, M4, M5, M6, M7, M8, M9, M10, M11, M12, M13, M14, M15, M16, M17, M18, M19, M20, M21, M22, M23, M24, M25, M26, M27, M28, M28, M29, M30, M31, M32, M33, M33, M34, M35, M36, M37, M38, M39, M40, M41, M42, M43, M44, M45, M46, M47, M48, M49, M50, M51, M52, M53, M54, M55, M56, M57, M58, M59, M60, M61, M62, M63, M64;
+function setup(){
+  canvas = createCanvas (400, 400);
+  M1 = createSprite(20, 50, 50, 10);
+  M2 = createSprite(100, 50, 50, 10);
+  M3 = createSprite(180, 50, 50, 10);
+  M4 = createSprite(180, 80, 10, 50);
+  M5 = createSprite(10, 200, 50, 10);
+  M6 = createSprite(10, 300, 50, 10);
+  M7 = createSprite(100, 300, 50, 10);
+  M8 = createSprite(100, 150, 10, 50);
+  M9 = createSprite(150, 200, 50, 10);
+  M10 = createSprite(200, 80, 50, 10);
+  M11 = createSprite(250, 80, 10, 50);
+  M12 = createSprite(320, 100, 50, 10);
+  M13 = createSprite(400, 50, 10, 50);
+  M14 = createSprite(300, 200, 50, 10);
+  M15 = createSprite(300, 200, 10, 50);
+  M16 = createSprite(300, 250, 50, 10);
+  M17 = createSprite(350, 300, 50, 10);
+  M18 = createSprite(400, 100, 10, 50);
+  M19 = createSprite(400, 4000, 50, 10);
+  M20 = createSprite(250, 150, 50, 10);
+  M21 = createSprite(225, 150, 10, 50);
+  M22 = createSprite(20, 125, 50, 10);
+  M23 = createSprite(40, 125, 10, 50);
+  M24 = createSprite(100, 125, 50, 10);
+  M25 = createSprite(100, 200, 50, 10);
+  M26 = createSprite(100, 225, 10, 50);
+}
+function draw() {
+  background("black")
+  drawSprites();
+  
+}
